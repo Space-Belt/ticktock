0ce656527082e72f8f2a9e1a0185ad66ccbcacd6
Space-Belt <dngurwn@gmail.com>
2025-06-12 12:56:27 +0900

    chore: 헤더 패딩 마진 설정 수정

--- src/components/TickTockHomeHeader.tsx
+++ src/components/TickTockHomeHeader.tsx
@@ -1,12 +1,10 @@
-import { Text, View } from 'react-native';
 import React from 'react';
+import { View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
 import Logo from '@assets/images/icon_ticktock_logo.svg';
 
-type Props = {};
-
-const TickTockHomeHeader = (props: Props) => {
+const TickTockHomeHeader = () => {
   return (
     <View style={styles.container}>
       <Logo width={37} height={36} />
@@ -21,5 +19,7 @@ const styles = StyleSheet.create(theme => ({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingVertical: 8,
+    marginBottom: 16,
   },
 }));
