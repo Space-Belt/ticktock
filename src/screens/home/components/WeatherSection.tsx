f0b3bbcedea3f7013d9a950e5987cf94a9dc4614
Space-Belt <dngurwn@gmail.com>
2025-06-19 13:17:25 +0900

    refactoring: 날씨에 맞춘 아이콘 노출 구현

--- src/screens/home/components/WeatherSection.tsx
+++ src/screens/home/components/WeatherSection.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getWeatherWithCity } from '@services/weather/weather';
 import { StyleSheet } from 'react-native-unistyles';
+import ItemWrapper from './ItemWrapper';
+import SunIcon from '@assets/images/icon_sunny.svg';
+import RainIcon from '@assets/images/icon_rainy.svg';
+import SnowIcon from '@assets/images/icon_snow.svg';
+import ThunderIcon from '@assets/images/icon_thunder.svg';
+import WindIcon from '@assets/images/icon_wind.svg';
+import CloudIcon from '@assets/images/icon_cloud.svg';
+import { WEATHER_CONDITIONS } from '@entities/weather';
 
 type Props = {};
 
@@ -13,18 +21,36 @@ const WeatherSection = (props: Props) => {
   });
 
   React.useEffect(() => {
-    console.log(data);
+    console.log(data?.weather);
     console.log(isLoading);
     console.log(error);
   }, [data, isLoading, error]);
 
+  const renderIcon = (code: number) => {
+    const IconComponent = WEATHER_CONDITIONS[code].icon;
+    if (IconComponent === 'Sun') return <SunIcon />;
+    if (IconComponent === 'Rain') return <RainIcon />;
+    if (IconComponent === 'Snow') return <SnowIcon />;
+    if (IconComponent === 'Thunder') return <ThunderIcon />;
+    if (IconComponent === 'Wind') return <WindIcon />;
+    if (IconComponent === 'Cloudy') return <CloudIcon />;
+  };
+
+  if (isLoading || error || data === undefined) {
+    return <></>;
+  }
+
   return (
-    <View>
-      <Text>weatheddrSection</Text>
-    </View>
+    <ItemWrapper title="날씨" styleProp={styles.wrapperStyle}>
+      {renderIcon(data.weather[0].id)}
+    </ItemWrapper>
   );
 };
 
 export default WeatherSection;
 
-const styles = StyleSheet.create(theme => ({}));
+const styles = StyleSheet.create(theme => ({
+  wrapperStyle: {
+    marginTop: 16,
+  },
+}));
