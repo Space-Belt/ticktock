1a5166a5443dfc0ba9882cef35d1ebbd11fc1d9e
Space-Belt <dngurwn@gmail.com>
2025-07-03 15:41:15 +0900

    feat: 헤더컴포넌트 생성

---
+++ src/screens/todo/components/TodoListHeader.tsx
@@ -0,0 +1,42 @@
+import { Pressable, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native-unistyles';
+
+import BackIcon from '@assets/images/icon_chevron_left.svg';
+import { Font } from '@styles/font';
+
+type Props = {
+  handleNavigation: () => void;
+  title?: string;
+  children?: React.ReactNode;
+};
+
+const TodoListHeader = ({ handleNavigation, title, children }: Props) => {
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={handleNavigation}>
+        <BackIcon width={30} height={30} style={styles.iconStyle} />
+      </Pressable>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <View>{children && children}</View>
+    </View>
+  );
+};
+
+export default TodoListHeader;
+
+const styles = StyleSheet.create(theme => ({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 8,
+    marginBottom: 16,
+  },
+  iconStyle: {
+    color: theme.colors.text.primary,
+  },
+  titleStyle: {
+    ...Font.bodyMediumBold,
+  },
+}));
