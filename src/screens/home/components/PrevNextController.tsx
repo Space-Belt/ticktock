455ecf83e02b19214dcd26502e0ff2e74dfc7422
Space-Belt <dngurwn@gmail.com>
2025-07-09 14:55:04 +0900

    refactoring: 날짜 조정 컨트롤러 커스텀 추가

--- src/screens/home/components/PrevNextController.tsx
+++ src/screens/home/components/PrevNextController.tsx
@@ -4,6 +4,7 @@ import { StyleSheet } from 'react-native-unistyles';
 
 import LeftIcon from '@assets/images/icon_chevron_left.svg';
 import RightIcon from '@assets/images/icon_chevron_right.svg';
+import { Font } from '@styles/font';
 
 type Props = {
   handlePrevClicked: () => void;
@@ -11,15 +12,41 @@ type Props = {
   gap?: number;
   text?: string;
   width?: number;
+  iconSize?: number;
+  textSize?: number;
 };
 
-const PrevNextController = ({ handlePrevClicked, handleNextClicked, gap, text, width }: Props) => {
+const PrevNextController = ({
+  handlePrevClicked,
+  handleNextClicked,
+  gap,
+  text,
+  width,
+  iconSize,
+  textSize,
+}: Props) => {
   return (
     <View style={styles.container(gap, width)}>
       <Pressable onPress={handlePrevClicked}>
-        <LeftIcon style={styles.iconStyle} />
+        <LeftIcon
+          width={iconSize ? iconSize : 24}
+          height={iconSize ? iconSize : 24}
+          style={styles.iconStyle}
+        />
       </Pressable>
-      {text && <Text style={styles.textStyle}>{text}</Text>}
+      {text && (
+        <Text
+          style={[
+            styles.textStyle,
+            {
+              fontSize: textSize !== undefined ? textSize : 12,
+              fontFamily:
+                textSize !== undefined ? Font.bodySmallBold.fontFamily : Font.bodySmall.fontFamily,
+            },
+          ]}>
+          {text}
+        </Text>
+      )}
       <Pressable onPress={handleNextClicked}>
         <RightIcon style={styles.iconStyle} />
       </Pressable>
@@ -33,14 +60,15 @@ const styles = StyleSheet.create(theme => ({
   container: (gap?: number, width?: number) => ({
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
     gap,
     width,
   }),
   iconStyle: {
     color: theme.colors.text.primary,
   },
-  textStyle: {
-    width: 100,
+  textStyle: (width?: number) => ({
+    width: width ? 150 : 100,
     textAlign: 'center',
-  },
+  }),
 }));
