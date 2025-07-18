5a708b00e05ef3b35217248d80dd61df25cd9fa7
Space-Belt <dngurwn@gmail.com>
2025-07-18 19:54:34 +0900

    chore: 카테고리컨텐트 스타일 수정 및 반복 요일 추가

--- src/screens/repeatTodo/RepeatTodoScreen.tsx
+++ src/screens/repeatTodo/RepeatTodoScreen.tsx
@@ -6,6 +6,10 @@ import TodoListHeader from '@screens/todo/components/TodoListHeader';
 import React from 'react';
 import { Pressable, useColorScheme, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
+import CategoryWithContent from './components/CategoryWithContent';
+import { Text } from 'react-native-gesture-handler';
+import { Font } from '@styles/font';
+import { getWeekdayInfo } from '@utils/dateUtil';
 
 type Props = {};
 
@@ -23,6 +27,8 @@ const RepeatTodoScreen = (props: Props) => {
       navigation.navigate('MainStack', { screen: 'AddTodo', params: params });
     }
   };
+
+  console.log(todo);
   return (
     <View>
       <TodoListHeader
@@ -36,10 +42,50 @@ const RepeatTodoScreen = (props: Props) => {
           </Pressable>
         }
       />
+      <View style={styles.wrapper}>
+        <CategoryWithContent
+          title={'할일'}
+          child={
+            <>
+              <Text style={styles.titleText}>{todo.title}</Text>
+            </>
+          }
+        />
+        <CategoryWithContent
+          title={'반복 요일'}
+          child={
+            <View style={styles.daysText}>
+              {todo?.repeatDays?.map(el => (
+                <Text style={styles.titleText(getWeekdayInfo(el).color)}>
+                  {getWeekdayInfo(el).label}
+                </Text>
+              ))}
+            </View>
+          }
+        />
+      </View>
     </View>
   );
 };
 
 export default RepeatTodoScreen;
 
-const styles = StyleSheet.create(theme => ({}));
+const styles = StyleSheet.create(theme => ({
+  wrapper: {
+    paddingHorizontal: 16,
+  },
+  titleText: (color: string) => ({
+    backgroundColor: color,
+    width: 30,
+    height: 30,
+    textAlign: 'center',
+    borderRadius: 15,
+    lineHeight: 30,
+    overflow: 'hidden',
+    ...Font.bodyMediumBold,
+  }),
+  daysText: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+}));
--- src/screens/repeatTodo/components/CategoryWithContent.tsx
+++ src/screens/repeatTodo/components/CategoryWithContent.tsx
@@ -1,15 +1,17 @@
 import { Text, View } from 'react-native';
 import React from 'react';
 import { StyleSheet } from 'react-native-unistyles';
+import { Font } from '@styles/font';
 
 type Props = {
   child: React.ReactNode;
+  title: string;
 };
 
-const CategoryWithContent = ({ child }: Props) => {
+const CategoryWithContent = ({ child, title }: Props) => {
   return (
     <View style={styles.container}>
-      <Text>CategoryWithContent</Text>
+      <Text style={styles.categoryText}>{title}</Text>
       {child}
     </View>
   );
@@ -20,5 +22,10 @@ export default CategoryWithContent;
 const styles = StyleSheet.create(theme => ({
   container: {
     gap: 8,
+    marginBottom: 12,
+  },
+  categoryText: {
+    ...Font.bodyLargeExtraBold,
+    color: theme.colors.text.primary,
   },
 }));
